46cdf327af0c9cfa75f9070f4d467ad572e23a65
rupesh6565 <rks121510@gmail.com>
2021-10-13 14:53:42 +0530

    changess7

--- nice_forum/settings.py
+++ nice_forum/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'vof)q^o6c1fvaq6^j92)lrfjd%xff*h*a2)1*=%#oy@q(qiykn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
